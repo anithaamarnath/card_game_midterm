9262f6658ecaac4b02f2da670f9d3cbd7d3e353a
karinalop <karina.popeye@gmail.com>
2019-04-26 18:50:46 +0000

    modify test_query

--- test_query.js
+++ test_query.js
@@ -9,15 +9,76 @@ var knex = require('knex')({
   }
 });
 
-const user_id = 1;
 
-knex.select('*').from('users')
-  // .where('first_name', '=', args)
-  // .orWhere('last_name', '=', args)
+
+// knex.select('*').from('users')
+//   // .where('first_name', '=', args)
+//   // .orWhere('last_name', '=', args)
+//   .asCallback(function(err, rows) {
+//     if(err){
+//       return console.error("Connection Error", err);
+//     }
+//     console.log(rows);
+//   });
+
+
+//------------query to select inactive games for an user---------------
+
+function archiveMatchesForUser(user_id){
+  knex.select('*').from('matches')
+  .where('player1_id', user_id)
+  .orWhere('player2_id',user_id)
+  .andWhere('game_state_id', 4)
   .asCallback(function(err, rows) {
     if(err){
       return console.error("Connection Error", err);
     }
+    console.log("Archive games:");
     console.log(rows);
   });
+}
+
+//--------------------------------------------------------
+
+function activeMatchesForUser(user_id){
+  knex.select('*').from('matches')
+  .whereNot('game_state_id', 4)
+  .andWhere('player1_id', user_id)
+  .orWhere('player2_id',user_id)
+  .asCallback(function(err, rows) {
+    if(err){
+      return console.error("Connection Error", err);
+    }
+    console.log("Active games:");
+    console.log(rows);
+  });
+}
+
+//-----------------------------------------------
+function active(user_id){
+  knex.select('*').from('matches')
+//   //.innerJoin('users', 'users.id', 'matches.player1_id')
+//   knex.select('*').from('matches').innerJoin('users', function() {
+//   this.on('users.id','=','matches.player1_id').orOn('users.id', '=','matches.player2_id')
+// })
+  .whereNot('game_state_id', 4)
+  .andWhere('player1_id', user_id)
+  .orWhere('player2_id',user_id)
+  .asCallback(function(err, rows) {
+    if(err){
+      return console.error("Connection Error", err);
+    }
+    console.log("Active games:");
+    console.log(rows);
+  });
+}
+//------------------------------------------------
+
+const user_id = 2;
+
+activeMatchesForUser(user_id);
+//active(user_id);
+
+
+
 
